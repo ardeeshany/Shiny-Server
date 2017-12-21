0cae6549425bde2fa4dae37b1906b4a9b6b86e5e
ardeeshany <ardeeshany@gmail.com>
2017-12-22 02:52:47 +0330

    basic functions

--- Intro template 01.R
+++ Intro template 01.R
@@ -1,7 +1,20 @@
 library(shiny)
+test<-function(n){
+  return(rnorm(n))
+}
 
-ui<-fluidPage()
+ui<-fluidPage(
+  sliderInput(inputId = "num",label = "Hello",min = 0,max = 100,value = 25),
+  plotOutput(outputId = "hist"),
+  tableOutput(outputId = "table")
+)
 
-server <- function(input,output){}
+server <- function(input,output){
+  output$hist <- renderPlot({
+    title <- "histogram"
+    hist(test(input$num) , main=title)
+    })
+  output$table <- renderTable(test(input$num))
+}
 
 shinyApp(ui = ui, server = server)
