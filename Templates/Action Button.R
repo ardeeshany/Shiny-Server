a4abc92df2fd83cc70d20bd117d818c3dc297dc1
ardeeshany <ardeeshany@gmail.com>
2017-12-23 18:32:41 +0330

    observeEvent, eventReactive, reactiveValues

--- Templates/Action Button.R
+++ Templates/Action Button.R
@@ -1,3 +1,32 @@
+#######################
+#######################
+#######################
+#######################
+#######################
+####################### observeEvent(): triggers code to run on the server
+library(shiny)
+
+ui<-fluidPage(
+  
+  sliderInput("x",label="Input",min = 0,max = 100,value = 5),
+  ## Action buttons  
+  actionButton(inputId = "go",label = "Action")
+)
+
+server <- function(input,output){
+
+  
+### by each click, it prints the input value in R session.     
+  observeEvent(input$go, {
+    print(as.numeric(input$x))
+  })
+
+  
+}
+shinyApp(ui = ui, server = server)
+
+
+
 
 
 
@@ -11,7 +40,7 @@
 #######################
 #######################
 #######################
-####################### eventReactive: to delay reactions until a user clicks the action button.
+####################### eventReactive(): to delay reactions until a user clicks the action button.
 library(shiny)
 
 ui<-fluidPage(
@@ -44,33 +73,27 @@ shinyApp(ui = ui, server = server)
 #######################
 #######################
 #######################
-#######################
-####################### reactiveValues() : To build several action buttons that control the "same object" (same location) ; combine obserEvent()   
+####################### Creat a list of reactive values
+####################### reactiveValues() : Manage state; To build several action buttons that control the "same object" (same location) ; combine obserEvent()   
 library(shiny)
 test<-function(n){
   return(rnorm(n))
 }
 
 ui<-fluidPage(
-  sliderInput(inputId = "num",label = "histogram",min = 0,max = 100,value = 25),
-  
+
   ## Action buttons  
   actionButton(inputId = "go1",label = "Uniform"),
   actionButton(inputId = "go2",label = "Normal"),
   actionButton(inputId = "reset",label = "Clear"),
   ##  
   
-  plotOutput(outputId = "hist"),
-  tableOutput(outputId = "table"),
   plotOutput("plot")
 )
 
 server <- function(input,output){
   
-  data <- reactive(test(input$num))
-  
-  ## Action Button
-  
+  # it can be more than one ractive values like : reactiveValues(data = NULL,data2=NULL)
   v <- reactiveValues(data = NULL)
   
   observeEvent(input$go1, {
@@ -79,23 +102,16 @@ server <- function(input,output){
   observeEvent(input$go2, {
     v$data <- rnorm(100)
   })
+
+  
   observeEvent(input$reset, {
     v$data <- NULL
   })  
-  
   output$plot <- renderPlot({
     if (is.null(v$data)) return()
     hist(v$data)
   })
-  
-  ##
-  
-  
-  output$hist <- renderPlot({
-    title <- "histogram"
-    hist(data() , main=title)
-  })
-  output$table <- renderTable(data())
+
 }
 shinyApp(ui = ui, server = server)
 
