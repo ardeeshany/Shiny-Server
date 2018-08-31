5db723bf3052f325b67d5070e1eaf903dd2d9513
ardeeshany <ardeeshany@gmail.com>
2018-08-31 23:03:04 +0430

    # persian character fixed in scatter module

--- Module/Sidebar.R
+++ Module/Sidebar.R
@@ -14,20 +14,20 @@ SidebarUI <- function(id){
               
       #sidebarSearchForm(textId = "searchText", buttonId = "searchButton",label = "Search..."),
 
-      #menuItem("ورود", tabName="Login", icon = icon("sign-in")),
-      #menuItem("خلاصه", tabName= "Summary", icon = icon("list-ul")),
+      menuItem("ورود", tabName="Login", icon = icon("sign-in")),
+      menuItem("خلاصه", tabName= "Summary", icon = icon("list-ul")),
       menuItem("کلاس",tabname= "Class", icon = icon("sitemap"),tabName = "Ref",
                menuSubItem("دوازدهم", tabName = "C12"),
                menuSubItem("یازدهم", tabName = "C11"),
                menuSubItem("دهم", tabName = "C10")),
-      #menuItem("دانش آموز", tabName = "Student", icon = icon("users","lg")),
-      #menuItem("دبیر", tabName = "Teacher", icon = icon("user-secret","fa-lg")),
-      #menuItem("Source code", icon = icon("file-code-o"),       # link to external content
-      #                 href = "https://github.com/ardeeshany/"),
+      menuItem("دانش آموز", tabName = "Student", icon = icon("users","lg")),
+      menuItem("دبیر", tabName = "Teacher", icon = icon("user-secret","fa-lg")),
+      menuItem("Source code", icon = icon("file-code-o"),       # link to external content
+                       href = "https://github.com/ardeeshany/"),
 
       ### Dynamic item : Instead of menuItem, use menuItemOutput
       ### It needs a render function in server function
-      # menuItemOutput(ns("menuitem")),
+       menuItemOutput(ns("menuitem")),
       #  tags$head(tags$style(HTML('.shiny-server-account { display: none; }'))),
 
       # The dynamically-generated user panel
